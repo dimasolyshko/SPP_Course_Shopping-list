e7cd5335645b09309b25baa71dd27780bba952eb
dimasolyshko <99644055+dimasolyshko@users.noreply.github.com>
2024-12-13 22:07:57 +0300

    Initial commit

---
+++ Shopping-list/Shopping-list/src/test/java/Disoland/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package Disoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
